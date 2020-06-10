5b3e8c71fc5d04699597be704dada6ec2ec0b427
Shivansh Saini <shivanshs9@gmail.com>
2020-06-11 01:51:42 +0530

    Working Request collector task

--- plugins/ergo/dags/dag_task_queuer.py
+++ plugins/ergo/dags/dag_task_queuer.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from airflow import DAG
 from airflow.configuration import conf
-from airflow.utils.dates import days_ago
+from airflow.utils import timezone
 from ergo import SECTION_NAME
 from ergo.operators.sqs.sqs_task_pusher import SqsTaskPusherOperator
 from ergo.sensors.task_requests_batcher import TaskRequestBatchSensor
@@ -18,14 +18,18 @@ default_args = {
     'email_on_retry': False,
     'retries': 10,
     'retry_delay': timedelta(minutes=2),
-    'start_date': days_ago(1),
-    'schedule_interval': timedelta(minutes=30),
+    'start_date': timezone.utcnow() - timedelta(minutes=20),
 }
 
 max_requests = conf.getint(SECTION_NAME, "max_task_requests", fallback=10)
 sqs_queue_url = conf.get(SECTION_NAME, "request_queue_url")
 
-with DAG('ergo_task_queuer', default_args=default_args, is_paused_upon_creation=False) as dag:
+with DAG(
+    'ergo_task_queuer',
+    default_args=default_args,
+    is_paused_upon_creation=False,
+    schedule_interval=timedelta(minutes=30)
+) as dag:
     collector = TaskRequestBatchSensor(
         task_id=TASK_ID_REQUEST_SENSOR,
         max_requests=max_requests,
--- plugins/ergo/sensors/task_requests_batcher.py
+++ plugins/ergo/sensors/task_requests_batcher.py
@@ -1,8 +1,9 @@
+from datetime import timedelta
+
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 from airflow.utils.db import provide_session
 from airflow.utils.decorators import apply_defaults
 from airflow.utils.state import State
-
 from ergo.models import ErgoTask
 
 
@@ -15,19 +16,27 @@ class TaskRequestBatchSensor(BaseSensorOperator):
         *args,
         **kwargs
     ):
+        poke_interval = kwargs.get('retry_delay', timedelta(minutes=2))
+        timeout = poke_interval * kwargs.get('retries', 10)
+        kwargs['soft_fail'] = True
+        kwargs['poke_interval'] = kwargs.get('poke_interval', poke_interval.total_seconds())
+        kwargs['timeout'] = kwargs.get('timeout', timeout.total_seconds())
         super().__init__(*args, **kwargs)
         self.max_requests = max_requests
         self.xcom_tasks_key = xcom_tasks_key
 
     @provide_session
     def poke(self, context, session=None):
+        self.log.info('Querying for %s tasks...', State.QUEUED)
         result = session.query(ErgoTask).filter_by(
             state=State.QUEUED
         ).limit(self.max_requests)
-        if result.count() < self.max_requests and context['ti'].is_eligible_to_retry():
+        result = list(result)
+        self.log.info(f'Query result: {result}')
+        if len(result) < self.max_requests and context['ti'].is_eligible_to_retry():
             return False
         elif result:
-            self.xcom_push(context, self.xcom_tasks_key, list(result))
+            self.xcom_push(context, self.xcom_tasks_key, result)
             return True
         else:
             return False
